7f745da11c6a277a7c2f3b14ebc5d077d133d048
Sandarva3 <Sandarva_email>
2024-01-11 09:17:42 +0545

    updates

--- samasya/urls.py
+++ samasya/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home_view, name='home'),
-    
+    path('aboutus', views.about_view, name='about'),
+    path('login', views.login_view, name='login'),
+    path('register', views.register_view, name='register'),
+    path('post', views.post_view, name='post'),
+    path('trending', views.trending_view, name ='trending'),
 ]
--- samasya/views.py
+++ samasya/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def home_view(request):
-    return HttpResponse("Welcome")
+    return HttpResponse("Home Page")
+
+
+def about_view(request):
+    return HttpResponse("About Page")
+
+
+def login_view(request):
+    return HttpResponse("Login Page")
+
+
+def register_view(request):
+    return HttpResponse("Register Page")
+
+
+def post_view(request):
+    return HttpResponse("Post Page")
+
+
+def trending_view(request):
+    return HttpResponse("Trending Page")
