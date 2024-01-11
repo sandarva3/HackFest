07404af626a94c857ae4e5f8cf43b3a78b561b2a
Sandarva3 <Sandarva_email>
2024-01-11 15:49:35 +0545

    Built Database and Forms Backend, first part

--- samasya/forms.py
+++ samasya/forms.py
@@ -1,2 +1,30 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django import forms
+from .models import Customer
+from django.contrib.auth.forms import UserCreationForm, User
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(max_length= 35,required=True)
+    phone = forms.CharField(max_length= 10, required=True)
+    class Meta:
+        model = user
+        fields = ("username", "email", "phone", "password1", "password2")
+
+    def clean_email(self):
+        email = self.cleaned_data["email"]
+        if Customer.objects.filter(email = email).exists():
+            raise forms.ValidationError("That Email already Exists, Please provide different Email")
+        return email
+    
+    def clean_phone(self):
+        email = self.cleaned_data["phone"]
+        if Customer.objects.filter(phone = phone).exists():
+            raise forms.ValidationError("That Phone number already Exists, Please provide different Email address")
+        return phone
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data["email"]
+        user.phone = self.cleaned_data["phone"]
+        if commit:
+            user.save()
+        return user
--- samasya/models.py
+++ samasya/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Customer(models.MOdel):
+class Customer(models.Model):
     user = models.OneToOneField(User, null=False, blank=False, on_delete=models.CASCADE)
     phone = models.TextField(max_length = 10, unique=True, null=False, blank = False)
-    email = models.CharField(max_length = 35, unique = True, null=True, blank=True)
-    
+    name = models.CharField(max_length=45, null=True)
+    email = models.EmailField(max_length = 35, unique = True, null=True, blank=True)
+
     def __str__(self):
         return self.name
 
