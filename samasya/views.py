eba238b7d232ec5ce48e55444805cba5ade1b846
Sandarva3 <Sandarva_email>
2024-01-12 09:37:38 +0545

    updates

--- samasya/views.py
+++ samasya/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import CustomUserCreationForm
-from django.contrib.auth import login
+from django.contrib import auth
+from django.contrib.auth import login, logout
 from .models import Customer, User
 
 
@@ -16,7 +17,22 @@ def about_view(request):
 
 
 def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = auth.authenticate(request, username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'login.html', {'error': 'Invalid Username or Password'})
+
     return render(request, 'login.html')
+
 
 
 def register_view(request):
