a6668cee0668e72ad12e99e2fd604fbe2f318aa6
Puspa Regmi <103326765+Puspa222@users.noreply.github.com>
2024-01-12 10:43:37 +0545

    Add files via upload

---
+++ static/js/script.js
@@ -0,0 +1,14 @@
+function clicked(){
+   let a=document.getElementById('cmt');
+   a.hidden=false;
+ 
+   let b=document.getElementById('cmtt');
+   b.hidden=true;
+}
+function back(){
+    let a=document.getElementById('cmt');
+    a.hidden=true;
+    let b=document.getElementById('cmtt');
+    b.hidden=false;
+   
+ }
