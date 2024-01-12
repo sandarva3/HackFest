2ea80f16f969785bd1189ad4f9160e74c9ad5c0b
Sandarva3 <Sandarva_email>
2024-01-12 13:25:43 +0545

    updates

--- samasya/admin.py
+++ samasya/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Customer
+from .models import Customer, Post, Comment
 
 admin.site.register(Customer)
+admin.site.register(Post)
+admin.site.register(Comment)
